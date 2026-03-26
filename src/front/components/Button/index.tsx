bd7e08578f13fffce9ac905e3dc95788a3ae8bbc
DJoverdant <andreestevesarantes19@gmail.com>
2026-03-25 21:29:48 -0300

    feat: add button component

--- src/front/components/Button/index.tsx
+++ src/front/components/Button/index.tsx
@@ -1,11 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
 import "./styles.css";
 
 interface ButtonProps { text: string };
 
 function Button({text}: ButtonProps) {
-    return (
-        <button className="button"> {text} </button>
-    );
+  return (
+    <button className="button">
+    <span>{text}</span>
+    </button>
+  );
 }
 
 export default Button;
